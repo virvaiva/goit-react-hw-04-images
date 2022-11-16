610e73741c81264cb7ef6270b1456ac886c4f99f
Oksana Konieva <anaska0489@gmail.com>
2022-11-16 11:39:23 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,40 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import * as SC from './Modal.styled';
 import { createPortal } from 'react-dom';
 
 const ModalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleDown);
-  }
-
-  handleDown = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  handleClick = event => {
+export function Modal({ onClose, children }) {
+  const handleCloseClick = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
+  useEffect(() => {
+    const handleDown = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleDown);
+    return () => window.removeEventListener('keydown', handleDown);
+  }, [onClose]);
 
-  render() {
-    return createPortal(
-      <SC.Overlay onClick={this.handleClick}>
-        <SC.Modal>{this.props.children}</SC.Modal>
-      </SC.Overlay>,
-      ModalRoot
-    );
-  }
+  // handleClick = event => {
+  //   if (event.currentTarget === event.target) {
+  //     this.props.onClose();
+  //   }
+  // };
+
+  return createPortal(
+    <SC.Overlay onClick={handleCloseClick}>
+      <SC.Modal>{children}</SC.Modal>
+    </SC.Overlay>,
+    ModalRoot
+  );
 }
 
 Modal.propTypes = {
-  handleClick: PropTypes.func,
+  handleClose: PropTypes.func,
 };
